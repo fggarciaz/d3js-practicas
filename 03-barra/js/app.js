6351f6b8f7ea9497120634a6573ddd4544c16004
Fabricio Garcia <fgarciazerda@gmail.com>
2022-06-18 22:27:43 -0500

    Barras-1 Gráficas de cabeza

--- 03-barra/js/app.js
+++ 03-barra/js/app.js
@@ -1,9 +1,49 @@
 const graf = d3.select("#graf")
 
-const anchoTotal = +graf.style('width').slice(0, 2);
+const anchoTotal = +graf.style('width').slice(0, -2);
 const altoTotal = (anchoTotal * 9) / 16
+
+const margins = {
+    top: 20,
+    right: 28,
+    bottom: 75,
+    left: 100
+}
+
+const ancho = anchoTotal - margins.left - margins.right;
+const alto = altoTotal - margins.top - margins.bottom;
 
 const svg = graf.append('svg')
     .attr('width', anchoTotal)
     .attr('height', altoTotal)
     .attr('class', 'graf')
+
+const layer = svg.append('g')
+    .attr('transform', `translate(${margins.left}, ${margins.top})`)
+
+layer.append('rect')
+    .attr('width', ancho)
+    .attr('height', alto)
+    .attr('fill', 'white')
+
+const g = svg.append('g')
+    .attr('transform', `translate(${margins.left}, ${margins.top})`)
+
+
+const load = async () => {
+    data = await d3.csv('barras.csv', d3.autoType)
+    console.log(data);
+
+    const rect = g.selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('x', (d, i) => 75 * i)
+        .attr('y', 0)
+        .attr('width', 50)
+        .attr('height', (d) => d.clientes)
+        .attr('fill', '#ffb703')
+
+}
+
+load()
