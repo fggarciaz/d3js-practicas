5db7b8f2195d7f4f65a11c3ae680e624c282fb6f
Fabricio Garcia <fgarciazerda@gmail.com>
2022-06-19 20:48:41 -0500

    Barras completas 2

--- 03-barra/js/app.js
+++ 03-barra/js/app.js
@@ -1,5 +1,6 @@
 // Selecciones
 const graf = d3.select("#graf")
+const metrica = d3.select('#metrica')
 
 // Dimensiones
 const anchoTotal = +graf.style('width').slice(0, -2);
@@ -34,61 +35,118 @@ const g = svg.append('g')
     .attr('transform', `translate(${margins.left}, ${margins.top})`)
 
 
-const load = async () => {
+const draw = async (variable = 'clientes') => {
     // Carga de datos
     data = await d3.csv('barras.csv', d3.autoType)
 
+    metrica.selectAll('option')
+        .data(Object.keys(data[0]).slice(1))
+        .enter()
+        .append('option')
+        .attr('value', (d) => d)
+        .text((d) => d)
+
     // Accesores
-    const yAccessor = (d) => d.clientes
     const xAccessor = (d) => d.tienda
 
-    // Ordenar datos
-    data.sort( (a, b) => yAccessor(b) - yAccessor(a) )
     
     // Escaladores
     const y = d3
         .scaleLinear()
-        .domain([0, d3.max(data, yAccessor)])
         .range([alto, 0])
+    
+    const color = d3
+        .scaleOrdinal()
+        .domain(Object.keys(data[0]).slice(1))
+        .range(d3.schemeTableau10)
+        //.range(["chocolate", "olive", "lightseagreen", "mediumpurple", "salmon"])
     
     const x = d3
         .scaleBand()
-        .domain(d3.map(data, xAccessor))
         .range([0, ancho])
         .paddingOuter(0.2)
         .paddingInner(0.1)
 
-    // Rectángulos (Elementos)
-    const rect = g.selectAll('rect')
-        .data(data)
-        .enter()
-        .append('rect')
-        .attr('x', (d) => x(xAccessor(d)))
-        .attr('y', (d) => y(yAccessor(d)))
-        .attr('width', x.bandwidth())
-        .attr('height', (d) => alto- y(yAccessor(d)))
-        .attr('fill', '#ffb703')
-    
-    // Titulos
-    g.append('text')
+    const titulo = g.append('text')
         .attr('x', ancho/2)
         .attr('y', -15)
         .classed('titulo', true)
-        .text('Ganancia de las tiendas')
-        
 
-    // Ejes
-    const xAxis = d3.axisBottom(x)
-    const yAxis = d3.axisLeft(y).ticks(8)
+    const etiquetas = g.append('g')
 
     const xAxisGroup = g
         .append('g')
         .attr('transform', `translate(0, ${alto})`)
         .classed('axis', true)
-        .call(xAxis)
 
-    const yAxisGroup = g.append('g').classed('axis', true).call(yAxis)
+    const yAxisGroup = g.append('g').classed('axis', true)
+
+    const render = (variable) => {
+        // Accesores
+        const yAccessor = (d) => d[variable]
+
+        // Ordenar datos
+        data.sort( (a, b) => yAccessor(b) - yAccessor(a) )
+
+        // Escaladores
+        y.domain([0, d3.max(data, yAccessor)])
+        x.domain(d3.map(data, xAccessor))
+
+        // Rectángulos (Elementos)
+        const rect = g.selectAll('rect').data(data, xAccessor)
+        rect.enter()
+            .append('rect')
+            .attr('x', (d) => x(xAccessor(d)))
+            .attr('y', (d) => y(0))
+            .attr('width', x.bandwidth())
+            .attr('height', 0)
+            .attr('fill', 'gray')
+            .merge(rect)
+            .transition()
+            .duration(1000)
+            //.ease(d3.easeBounce)
+            .attr('x', (d) => x(xAccessor(d)))
+            .attr('y', (d) => y(yAccessor(d)))
+            .attr('width', x.bandwidth())
+            .attr('height', (d) => alto- y(yAccessor(d)))
+            .attr('fill', (d) => xAccessor(d) == 'Satélite' ? 'darkred' : color(variable))
+
+        const et = etiquetas.selectAll('text').data(data)
+        et.enter()
+            .append('text')
+            .attr('x', (d) => x(xAccessor(d)) + x.bandwidth()/2 )
+            .attr('y', (d) => y(0))
+            .merge(et)
+            .transition()
+            .duration(1000)
+            .attr('x', (d) => x(xAccessor(d)) + x.bandwidth()/2 )
+            .attr('y', (d) => y(yAccessor(d)))
+            .text(yAccessor)
+
+
+        // Titulos
+        titulo.text(`${variable} de las tiendas`)
+            
+
+        // Ejes
+        const xAxis = d3.axisBottom(x)
+        const yAxis = d3.axisLeft(y).ticks(8)
+        yAxisGroup.transition().duration(1000).call(yAxis)
+        xAxisGroup.transition().duration(1000).call(xAxis)
+
+
+        }
+
+        // Eventos
+        metrica.on('change', (e) => {
+            e.preventDefault()
+            // console.log(e.target.value, metrica.node().value);
+            render(e.target.value)
+        } )
+
+        render(variable)
+
 
 }
 
-load()
+draw('clientes')
